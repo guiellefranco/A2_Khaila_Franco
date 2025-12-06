8b672c8a1816381fc2db20a89b6cf957d39c9a21
guiellefranco <guielle.franco@gmail.com>
2025-12-06 00:02:26 -0500

    Network error message

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -50,7 +50,7 @@ export default function MainScreen() {
       setConverted((Number(amount) * r).toFixed(2));
 
     } catch (e) {
-      setError("Network error or invalid API key.");
+      setError("Network error");
     } finally {
       setLoading(false);
     }
